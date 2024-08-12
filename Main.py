06dc5a7d66057ba1fcfd0ed08a9335479089a5aa
MikaelUp <mikaelsalesbarros@gmail.com>
2024-08-12 16:45:57 -0300

    Criação do primeiro frame

--- Main.py
+++ Main.py
@@ -7,16 +7,18 @@ import os
 
 
 # Criar a janela e o tema gráfico
-interface = ttk.Window(title="Afastamento", themename="darkly")
+interface = ttk.Window(title="Afastamento", themename="cosmo")
 interface.geometry("850x620")
 
 # Tirando a opção de redimensionar a janela
 interface.resizable(False, False)
 
 
+# Criando Frames
+frame_logo = ttk.Frame(interface, width=850, height=52, bootstyle="primary")
+frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 
-
-
+ttk.Separator(interface, orient=HORIZONTAL).grid(row=1, column=0, ipadx=680, pady=0)
 
 
 
